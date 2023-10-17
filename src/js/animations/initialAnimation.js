c9a6795b8a689bcadcbfc2040ee1ed5df4e6d435
Bekarion <beqarioni@yahoo.co.uk>
2023-10-18 02:09:51 +0400

    asdf

--- src/js/animations/initialAnimation.js
+++ src/js/animations/initialAnimation.js
@@ -11,24 +11,39 @@ export default function initialAnimation() {
       opacity: [0, 1],
       translateY: ["-100%", 0],
     })
-    .add({
-      targets: "#submit-btn",
-      easing: "easeOutExpo",
-      opacity: [0, 1],
-      "margin-right": ["100vw", 0],
-      rotateZ: [-720, 0],
-    })
-    .add({
-      targets: "#results p",
-      opacity: [0, 1],
-      translateX: ["100%", 0],
-    })
-    .add({
-      targets: "#results span",
-      opacity: [0, 1],
-      translateY: ["-100%", 0],
-    })
-    .add({
-      targets: ".labels",
-    })
+    .add(
+      {
+        targets: "#submit-btn",
+        easing: "easeOutExpo",
+        opacity: [0, 1],
+        "margin-right": ["100vw", 0],
+        rotateZ: [-720, 0],
+      },
+      "-=500"
+    )
+    .add(
+      {
+        targets: "#results p",
+        opacity: [0, 1],
+        translateX: ["100%", 0],
+      },
+      "-=500"
+    )
+    .add(
+      {
+        targets: "#results span",
+        opacity: [0, 1],
+        translateY: ["-100%", 0],
+      },
+      "-=500"
+    )
+    .add(
+      {
+        targets: ".labels",
+        translateY: [0, "-15%", "3%", 0],
+        delay: anime.stagger(100),
+        easing: "linear",
+      },
+      "-=980"
+    )
 }
